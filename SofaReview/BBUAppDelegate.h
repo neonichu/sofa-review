ecb780f450a754b2bb1f521083191419442de06b
Boris Bügling <boris@icculus.org>
2013-05-11 10:10:13 +0200

    Initial Commit

---
+++ SofaReview/BBUAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BBUAppDelegate.h
+//  SofaReview
+//
+//  Created by Boris Bügling on 11.05.13.
+//  Copyright (c) 2013 Boris Bügling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BBUAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
